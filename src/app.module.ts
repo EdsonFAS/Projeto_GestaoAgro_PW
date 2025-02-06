324fc82129a2813298f8c4462b280e8ef39cd295
Edson Fernando Souza Bezerra <2022202060017@aluno.ifro.edu.br>
2025-02-05 20:28:29 -0400

    feat: add page, service, controller for home

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SobreModule } from './sobre/sobre.module';
 import { LoginModule } from './login/login.module';
+import { HomeModule } from './home/home.module';
 
 @Module({
-  imports: [SobreModule, LoginModule],
+  imports: [SobreModule, LoginModule, HomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
